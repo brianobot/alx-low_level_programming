5df1a64a6377a633e091f05dd4cbad3170f78fba
Brian Obot <brianobot9@gmail.com>
2022-10-13 21:36:59 -0700

    Task 4, 5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: 0 after printing the function
+ *     */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
