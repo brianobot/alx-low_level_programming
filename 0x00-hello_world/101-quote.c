2216603d10e45fc4e30e0b26acc237a83af5d0ab
Brian Obot <brianobot9@gmail.com>
2022-10-14 05:52:49 +0100

    Update task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main -entry point
+ * main - Entry point
  *
- * Return: 1 on success
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
