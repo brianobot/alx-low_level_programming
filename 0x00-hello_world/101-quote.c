e311ddde8339f52e6f8d93112bafafae155293c2
Brian Obot <brianobot9@gmail.com>
2022-10-14 05:48:06 +0100

    Add task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main -entry point
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
