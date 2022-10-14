4fdca47df6bcd56a443ed744ec2761412db1e177
Brian Obot <brianobot9@gmail.com>
2022-10-14 05:40:32 +0100

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   *
- *    * Return: 0 after printing the function
- *     */
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
