375f980f1b45b2eb2efd706d3d9669d70bee605c
Brian Obot <brianobot9@gmail.com>
2022-10-14 05:39:21 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *  *  main - entry point
- *   *
- *    *  Return: 0 after printing the function
- *     */
+ *  main - entry point
+ *
+ *  Return: 0 after printing the function
+ */
 int main(void)
 {
-		char string[] = "\"Programming is like building a multilingual puzzle";
+	char string[] = "\"Programming is like building a multilingual puzzle";
 
-			puts(string);
-				return (0);
+	puts(string);
+	return (0);
 }
