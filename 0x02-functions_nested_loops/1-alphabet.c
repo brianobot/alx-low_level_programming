e9f6ce2216728963254c55d349139f79c736a6ca
Brian Obot <brianobot9@gmail.com>
2022-10-20 05:04:13 -0700

    Add task 1 and 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-*print_alphabet - Entry point
-*Description: a function that prints the alphabet,
-*in lowecase followed by a new line
-*Return: void
-*/
+ * *print_alphabet - Entry point
+ * *Description: a function that prints the alphabet,
+ * *in lowecase followed by a new line
+ * *Return: void
+ * */
 void print_alphabet(void)
 {
-char letter = 'a';
+	char letter = 'a';
 
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - func
- *
- * Return - 0
- */
-void print_alphabet(void)
+ *  * print_alphabet_x10 - func
+ *   *
+ *    * Return - 0
+ *     */
+void print_alphabet_x10(void)
 {
-int n = 0;
-while (n < 10)
-{
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
-}
-_putchar('\n');
-n++;
-}
+	int n = 0;
+	while (n < 10)
+	{
+		char a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		n++;
+	}
 }
