13bbf035344e3c50b757d6d88eea46eda07858e7
Brian Obot <brianobot9@gmail.com>
2022-10-24 15:58:26 -0700

    Add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_rev - takes a string and prints it in reverse
+ *
+ * @s first parameter
+ *
+ * Return: void
+ *//
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
