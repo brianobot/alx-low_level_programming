7a7990bbfbc2158d58f227247c5206310091c6d2
Brian Obot <brianobot9@gmail.com>
2022-10-24 15:26:27 -0700

    Add task 0

---
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98  takes a pointer to an int parameter
+ * 		and updates the value ot points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
