706bd7a692f981d4f96477d1e039a96fccbd4bf3
Brian Obot <brianobot9@gmail.com>
2022-10-24 16:24:33 -0700

    Add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: parameter input for a
+ *
+ * Return: nothinh
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char ch;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[l -1 - i];
+		s[l -1 -i] = ch;
+	}
+}
