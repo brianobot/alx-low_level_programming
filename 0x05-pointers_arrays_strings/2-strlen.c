be5771b2b9dbb53f34c599a91896d349699bc801
Brian Obot <brianobot9@gmail.com>
2022-10-24 15:43:48 -0700

    Add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - takess a string as argument and return it length
+ *
+ * @s: first parameter
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(*s);
+
+	return (len);
+}
