47511204efb344109e93b255d0fdc2e3a1530283
Brian Obot <brianobot9@gmail.com>
2022-10-24 15:49:22 -0700

    Add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts - printrs a string to the standard output followed by a newline
+ *
+ * @str: parameter to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str);
+{
+	printf("%s\n",str);
+}
