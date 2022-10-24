493683e659d442cd4cc5995262c285e62edec9aa
Brian Obot <brianobot9@gmail.com>
2022-10-24 13:18:00 -0700

    Add task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *print_alphabet - Entry point
- * *Description: a function that prints the alphabet,
- * *in lowecase followed by a new line
- * *Return: void
- * */
+ *print_alphabet - Entry point
+ *Description: a function that prints the alphabet,
+ *in lowecase followed by a new line
+ *Return: void
+ */
 void print_alphabet(void)
 {
 	char letter = 'a';
