344c850b403c267e4502215ae917600397a30c55
Brian Obot <brianobot9@gmail.com>
2022-10-24 16:52:01 -0700

    Add task 9

---
+++ 0x05-pointers_arrays_strings/9-strcopy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by the src
+ *
+ * @src: source of string parameter input
+ * @dest: destinatio of string
+ *
+ * Return: Pointer to the dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do 
+	{
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
