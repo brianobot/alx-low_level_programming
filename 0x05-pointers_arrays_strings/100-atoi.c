9444b6694a1b088cd046ab7ec49152052101f5e0
Brian Obot <brianobot9@gmail.com>
2022-10-24 17:01:15 -0700

    Add task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 			sign *= -1;
-		else if (*s >= 0' && *s <= '9')
+		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
