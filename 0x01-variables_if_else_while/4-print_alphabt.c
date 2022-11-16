7b1ce28a68159962d0bf747fb343261175e1c9f1
Brian Obot <brianobot9@gmail.com>
2022-11-15 22:25:08 -0800

    Add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function beginds program execution
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	char lower_base = 'a';
+	char lower_size = lower_base + 25;
+	int i;
+
+	for (i = lower_base; i <= lower_size; ++i)
+	{
+		if (i == 'e' || i == 'q')
+		       continue;
+		putchar(i);	
+	}
+	return (0);
+}
