66c25947f15defcd1a69093e2448af3c35dff73f
Brian Obot <brianobot9@gmail.com>
2022-11-15 22:07:28 -0800

    Add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function starts program execution
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	char a = 'a';
+	unsigned int i;
+
+	for (i = a, i < (a + 25); ++i)
+	{
+		putchar(index);
+	}
+	putchar('\n');
+	return (0);
+}
