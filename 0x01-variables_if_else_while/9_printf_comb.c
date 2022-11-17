4d2936cc9b6da4d68a0c0b019d5b785c4937c863
Brian Obot <brianobot9@gmail.com>
2022-11-16 20:43:24 -0800

    Add task 9

---
+++ 0x01-variables_if_else_while/9_printf_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - starts program execution
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	int base = 48;
+	int i;
+
+	for (i = base; i < (base + 10); ++i)
+	{
+		putchar(i);
+
+		if (i == (base + 9))
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n')
+	
+	return (0);
+}
