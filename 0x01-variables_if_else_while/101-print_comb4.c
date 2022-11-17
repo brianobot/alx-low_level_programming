f8a2e8e6183acbe89e3ca41fa8d5fffa7fcd6059
Brian Obot <brianobot9@gmail.com>
2022-11-16 22:56:26 -0800

    Add task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,8 +15,21 @@ int main(void)
 	{
 		for (j = base + 1; j < (base + 10); ++j)
 		{
-			for (k = base + 
+			for (k = base + 1; k < (base + 10); ++k)
+			{
+				if ( (i == j) || (i == k) || (j == k) || (i > j) || (i > k) || (j > k))
+					continue;
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}	
 		}
 	}
+	putchar('\n');
 	return (0);
 }
