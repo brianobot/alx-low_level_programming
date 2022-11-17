b543f92ef51ee081c717f0ae204ed5f1ce9ff35c
Brian Obot <brianobot9@gmail.com>
2022-11-17 15:34:27 -0800

    Add new task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of program
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; ++a)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b)
+	return (0);
+}
