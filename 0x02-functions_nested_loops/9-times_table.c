964c3f4594a90809a81dd3e89d80d15148dc7d49
Brian Obot <brianobot9@gmail.com>
2022-11-17 10:40:33 -0800

    Add task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		for (b = 0; b <= 9; ++b)
 		{
 			c = a * b;
-			if ((c / 10 ) == 0)
+			if ((c / 10) == 0)
 			{
 				if (b == 0)
 				{
@@ -34,7 +34,7 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			else 
+			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
@@ -43,7 +43,7 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-				}	
+				}
 			}
 		}
 		_putchar('\n');
