359a87d9736f836dc1d15ac2e7c5f732f64e6f90
Brian Obot <brianobot9@gmail.com>
2022-11-17 14:29:48 -0800

    Add task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				{
 					_putchar(((op / 100) % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
-					_putchar(((op % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
 			}
 			_putchar('\n');
