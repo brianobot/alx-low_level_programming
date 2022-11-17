e26f4aadd81602aa33aa3d6569cdeb85051a9627
Brian Obot <brianobot9@gmail.com>
2022-11-17 01:03:48 -0800

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - main entry point
+ * description: computes the absolute value of a num
+ * @int: integer argument
+ *
+ * Return: integer
+ */
+
+int _asb(int number)
+{
+	int val;
+
+	if (val >= 0)
+		return (number);
+	val = (int) number / -1;
+
+	return (val);
+}
