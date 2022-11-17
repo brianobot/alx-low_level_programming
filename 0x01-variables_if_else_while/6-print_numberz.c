58aa439facac52a3f15242d927f7f48a96d1244d
Brian Obot <brianobot9@gmail.com>
2022-11-16 20:15:46 -0800

    Ad task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starts program execution
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	int base = 0;
+	int i;
+
+	for (i = base; i < (base + 10); ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
