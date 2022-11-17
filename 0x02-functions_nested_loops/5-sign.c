1ca5a9fe75db0e96db4fe1af1b614d915423ba92
Brian Obot <brianobot9@gmail.com>
2022-11-17 09:00:10 -0800

    Ad task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,19 @@
 
 int print_sign(int n)
 {
-	int sign;
-
 	if (n > 0)
 	{
-		_putchar(43);
-		sign = 1;
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(48);
-		sign = 0;
+		_putchar('0');
+		return (0);
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
-		_putchar(45);
-		sign = -1;
+		_putchar('-');
+		return (-1);
 	}
-
-	return (sign);
 }
