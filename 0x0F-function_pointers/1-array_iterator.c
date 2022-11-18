0db8d5d73785972e349efed7859d6af4f63847b4
Brian Obot <brianobot9@gmail.com>
2022-11-17 23:40:54 -0800

    Add new task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - perform an action on a whole array
+ * @array: array to act on
+ * @size: size of array
+ * @action: function to execute on array
+ *
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action != NULL && array != NULL)
+	{
+		for (i = 0; i < size; ++i)
+			action(array[i]);
+	}
+}
