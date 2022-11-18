c0339622621d476885143c6dfedf3dee2edf3ff5
Brian Obot <brianobot9@gmail.com>
2022-11-17 23:37:09 -0800

    Add new task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - prints a name
  * @name: the name to be printed
- * @f: a pointer to afunction that print a name
+ * @f: a pointer to a function that print a name
  */
 
 void print_name(char *name, void (*f)(char *))
