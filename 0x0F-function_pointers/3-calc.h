c8a195436b923e81499cbfcca1c0e207221f7a4d
Brian Obot <brianobot9@gmail.com>
2022-11-18 07:00:22 -0800

    Add new task

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -18,6 +18,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_fun(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,3 +24,4 @@ int (*get_op_func(char *s))(int, int)
 		i++;
 
 	return (ops[i].f);
+}
