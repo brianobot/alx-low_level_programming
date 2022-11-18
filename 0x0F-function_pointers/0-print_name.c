150cbec2528ddfac8291f74f35e4fe673e4372a6
Brian Obot <brianobot9@gmail.com>
2022-11-17 23:35:45 -0800

    Add new task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -6,10 +6,10 @@
  * @f: a pointer to afunction that print a name
  */
 
-void print)name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
 
-	f(name);
+	(*f)(name);
 }
