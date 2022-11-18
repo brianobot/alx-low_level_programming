8bcc89df6cac8f41c012d58f63033f8e6df49394
Brian Obot <brianobot9@gmail.com>
2022-11-17 23:45:10 -0800

    Add new task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * int_index - checks an array for an integer that matches
+ * @array: array to search
+ * @size: size of array
+ * @cmp: comparison function to check with
+ *
+ * Return: index of the match or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp != NULL && array != NULL)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
