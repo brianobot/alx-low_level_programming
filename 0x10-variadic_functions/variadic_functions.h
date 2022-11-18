742d1ece32f311e5fde4a6fc090b7615aa6f7020
Brian Obot <brianobot9@gmail.com>
2022-11-18 07:55:05 -0800

    Add new task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,5 +20,6 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _puchat(char c);
 
 #endif
