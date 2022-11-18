2adb50eea3d85f50936f4f2fe9f80c692ea916b7
Brian Obot <brianobot9@gmail.com>
2022-11-17 22:51:43 -0800

    Add new task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,35 @@
 
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0;
-	unsigned long fib2 = 1;
-	unsigned long sum;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (count = 0; count < 98; count++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; ++i)
 	{
-		sum = fib1 + fib2;
-		printf("%lu", sum);
-		fib1 = fib2;
-		fib2 = sum;
-
-		if (count == 97)
-			printf("\n");
-		else
-			printf(", ");
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(" %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+	printf("\n");
+
 	return (0);
 }
