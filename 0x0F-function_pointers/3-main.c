fa70f188b26516b1f51fe7ca1498cecbef86b404
Brian Obot <brianobot9@gmail.com>
2022-11-18 07:29:44 -0800

    Add new task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,9 +9,10 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*func_ptr)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,21 +20,25 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	num1 =  atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	func_ptr = get_op_func(argv[2]);
 
-	if (func_ptr == NULL)
+	if ((*op == '/' && num2) == 0 ||
+		(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
